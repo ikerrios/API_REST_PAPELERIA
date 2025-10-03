7d76965637c4623209958110c6d40d966148da07
ikerrios <1925184@alu.murciaeduca.es>
2025-10-03 21:11:51 +0200

    frontend
    Creación del fronted y las cosas que contendran la tabla, debo modificarlo ya que he hecho copia y pega de la que hicimos con cipriano
    - Terminar la $url
    - Continuar con la tabla

---
+++ frontend/index.php
@@ -0,0 +1,26 @@
+<?php
+    $url = "https://localhost:8080/" //terminar esta línea.
+    $data = file_get_contents($url);
+    $articulos = json_decode($data, true);
+
+    //var_dump($articulos)
+?>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <title>Listado de artículos</title>
+    </head>
+    <body>
+        <table class="table table-striped">
+                <thead>
+                <h1>Listado de artículos</h1>
+                    <tr>
+                        <td>ID</td>
+                        <td>name</td>
+                        <td>chip</td>
+                        <td>category</td>
+                        <td>born</td>
+                        <td>adopt</td>
+                    </tr>
+                </thead>
+                <!--Continuar--> 
